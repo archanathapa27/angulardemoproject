be245d9375ba24430457dd8b96dd47322131249a
Archana Thapa <archana27.thapa@gmail.com>
2026-08-18 00:04:56 +0530

    updated the karma config for running test

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -40,5 +40,13 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
+    customLaunchers: {
+          ChromeHeadlessCI: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox', '--disable-gpu']
+          }
+        },
+        browsers: ['ChromeHeadlessCI'],
+        singleRun: true
   });
 };
